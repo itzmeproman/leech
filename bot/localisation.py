6d18626f4aaafc2fc11dbf0d7ef23acce46ec0e3
itzmeproman <atulpandey3030@gmail.com>
2024-05-14 12:45:14 +0530

    Add files via upload

---
+++ bot/localisation.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @AbirHasan2005
+
+from bot.get_cfg import get_config
+
+
+class Localisation:
+    START_TEXT = "Hello, \n\nThis is a Telegram Video Encoder Bot. Please send me any Telegram big video file I will compress it as s small video file! \n\n✨ /help for more details."
+   
+    ABS_TEXT = " Please don't be selfish."
+    
+    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
+    
+    
+    DOWNLOAD_START = "Downloading ... 📥 \n"
+    
+    UPLOAD_START = "Uploading ... 📤 \n"
+    
+    COMPRESS_START = "Encoding ... 📀"
+    
+    RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
+    
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
+    
+    COMPRESS_SUCCESS = "Encoded by Anime Sensei"
+
+    COMPRESS_PROGRESS = "⏳ ETA: {}\n🚀 Progress: {}%"
+
+    SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
+    
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Custom thumbnail cleared succesfully...✅"
+    
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully... ✅"
+    
+    SAVED_RECVD_DOC_FILE = "Downloaded Successfully...✅"
+    
+    CUSTOM_CAPTION_UL_FILE = " "
+    
+    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found...💔"
+    
+    NO_VOID_FORMAT_FOUND = "no-one gonna help you\n{}"
+    
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    
+    FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "⚠️ Already one Process going on! ⚠️ \n\nCheck Live Status on Anime Sensei Logs."
+    
+    HELP_MESSAGE = get_config(
+        "STRINGS_HELP_MESSAGE",
+        "Hi, I am Video Compressor Bot."
+    )
+    WRONG_MESSAGE = get_config(
+        "STRINGS_WRONG_MESSAGE",
+        "current CHAT ID: <code>{CHAT_ID}</code>"
+    )
+
+    
